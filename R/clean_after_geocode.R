511dd55181fa6b5849d9b6c57eeed9bf54546427
Simon Coulombe <simoncoulombe@gmail.com>
2022-01-14 15:00:49 -0500

    geocode csv

--- R/clean_after_geocode.R
+++ R/clean_after_geocode.R
@@ -3,24 +3,60 @@ library(readr)
 library(janitor)
 source("R/col_types_wells.R")
 
-library(RPostgres)
-con1 <- DBI::dbConnect(
-  RPostgres::Postgres(),
-  dbname = Sys.getenv("BCGOV_DB"),
-  host = Sys.getenv("BCGOV_HOST"),
-  user = Sys.getenv("BCGOV_USR"),
-  password=Sys.getenv("BCGOV_PWD")
-)
+update_sql <- FALSE
+update_csv <- TRUE
 
-# this is the list of well we geocoded today
-newly_geocoded <- read_csv("data/wells_geocoded.csv", col_types = col_types_geocoded) %>% 
-  mutate(date_geocoded = as.Date(Sys.time() , tz = "America/Vancouver")) %>%
-  janitor::clean_names()
-
-if(nrow(newly_geocoded)> 0){
-  message("printing newly geocoded")
-  glimpse(newly_geocoded)
-  message("Appending newly geocoded  wells:", nrow(newly_geocoded), " rows. well_tag_number=", paste(newly_geocoded$well_tag_number, collase = " "))
-  dbAppendTable(con1, "wells_geocoded", newly_geocoded)
+if(update_sql){
   
-} else{message("No new geocodedwells to append.")}
+  library(RPostgres)
+  con1 <- DBI::dbConnect(
+    RPostgres::Postgres(),
+    dbname = Sys.getenv("BCGOV_DB"),
+    host = Sys.getenv("BCGOV_HOST"),
+    user = Sys.getenv("BCGOV_USR"),
+    password=Sys.getenv("BCGOV_PWD")
+  )
+  
+  # this is the list of well we geocoded today
+  newly_geocoded <- read_csv("data/wells_geocoded.csv", col_types = col_types_geocoded) %>% 
+    mutate(date_geocoded = as.Date(Sys.time() , tz = "America/Vancouver")) %>%
+    janitor::clean_names()
+  
+  if(nrow(newly_geocoded)> 0){
+    message("printing newly geocoded")
+    glimpse(newly_geocoded)
+    message("Appending newly geocoded  wells:", nrow(newly_geocoded), " rows. well_tag_number=", paste(newly_geocoded$well_tag_number, collase = " "))
+    dbAppendTable(con1, "wells_geocoded", newly_geocoded)
+    
+  } else{message("No new geocodedwells to append.")}
+}
+
+if(update_csv){
+  # this is the list of geocoded csv on the repo 
+  geocoded_csv <-       read_csv("https://raw.githubusercontent.com/bcgov/GWELLS-QAQC_Geocode_ArchiveData/main/data/wells_geocoded.csv",
+                                 col_types = col_types_geocoded_janitor
+  )
+  
+  # this is the list of well we geocoded today
+  newly_geocoded <- read_csv("data/wells_geocoded.csv", col_types = col_types_geocoded) %>% 
+    mutate(date_geocoded = as.Date(Sys.time() , tz = "America/Vancouver")) %>%
+    janitor::clean_names()
+  
+  if(nrow(newly_geocoded)> 0){
+    message("printing newly geocoded")
+    glimpse(newly_geocoded)
+    message("Appending newly geocoded  wells:", nrow(newly_geocoded), " rows. well_tag_number=", paste(newly_geocoded$well_tag_number, collase = " "))
+    updated_wells_geocoded <- bind_rows(geocoded_csv, newly_geocoded)
+    write_csv(updated_wells_geocoded, "data/wells_geocoded.csv")
+    
+  } else{
+    message("No new geocodedwells to append.")
+    write_csv(geocoded_csv, "data/wells_geocoded.csv")
+    }
+  
+
+  
+  
+  
+  
+}
--- R/prep_geocode.R
+++ R/prep_geocode.R
@@ -3,46 +3,107 @@ library(readr)
 library(janitor)
 library(RPostgres)
 source("R/col_types_wells.R")
-con1 <- DBI::dbConnect(
-  RPostgres::Postgres(),
-  dbname = Sys.getenv("BCGOV_DB"),
-  host = Sys.getenv("BCGOV_HOST"),
-  user = Sys.getenv("BCGOV_USR"),
-  password=Sys.getenv("BCGOV_PWD")
-)
 
-max_number_geocoded <- 1000 # this is the maximum number of wells we are willing to geocode today
-source("R/col_types_wells.R")
+update_sql <- FALSE  # do we update the data on the SQL database?
+update_csv <- TRUE   # do we update the data in the CSV in the repo?
+
+if(update_sql){
+  con1 <- DBI::dbConnect(
+    RPostgres::Postgres(),
+    dbname = Sys.getenv("BCGOV_DB"),
+    host = Sys.getenv("BCGOV_HOST"),
+    user = Sys.getenv("BCGOV_USR"),
+    password=Sys.getenv("BCGOV_PWD")
+  )
+  
+  max_number_geocoded <- 1000 # this is the maximum number of wells we are willing to geocode today
+  source("R/col_types_wells.R")
+  
+  
+  # read the historical caracteristics
+  #gwells_data_first_appearance <- read_csv("data/gwells_data_first_appearance.csv", col_types = col_types_wells)
+  wells_in_db <- dbGetQuery(con1, "select well_tag_number, latitude_decdeg, longitude_decdeg  from wells") 
+  
+  
+  # read the list of wells that have already been geocoded  
+  #geocoded <- read_csv("github_data/wells_geocoded.csv", col_types = col_types_geocoded)
+  wells_geocoded_in_db <- dbGetQuery(con1, "select well_tag_number from wells_geocoded") 
+  
+  
+  # this is the list of wells that need geocoding
+  to_geocode <- wells_in_db %>%
+    anti_join(wells_geocoded_in_db) %>%
+    filter(!is.na(latitude_decdeg), !is.na(longitude_decdeg)) %>% 
+    tail(max_number_geocoded)
+  
+  
+  message(paste0(Sys.time() , "created to_geocode"))
+  
+  
+  # since the geocode python script reads data/wells.csv, we overwrite the 
+  # wells.csv file with the data we want to geocode.
+  write.csv(to_geocode %>%
+              rename(latitude_Decdeg = latitude_decdeg, 
+                     longitude_Decdeg = longitude_decdeg
+              )
+            , "data/wells.csv")
+  
+  if(nrow(to_geocode)>0){
+    message("Geocoding ", nrow(to_geocode)," rows. well_tag_number= ", paste(to_geocode$well_tag_number, collapse = " "))
+  } else {message("no new wells to geocode")}
+  
+}
 
 
-# read the historical caracteristics
-#gwells_data_first_appearance <- read_csv("data/gwells_data_first_appearance.csv", col_types = col_types_wells)
-wells_in_db <- dbGetQuery(con1, "select well_tag_number, latitude_decdeg, longitude_decdeg  from wells") 
 
-
-# read the list of wells that have already been geocoded  
-#geocoded <- read_csv("github_data/wells_geocoded.csv", col_types = col_types_geocoded)
-wells_geocoded_in_db <- dbGetQuery(con1, "select well_tag_number from wells_geocoded") 
-
-
-# this is the list of wells that need geocoding
-to_geocode <- wells_in_db %>%
-  anti_join(wells_geocoded_in_db) %>%
-  filter(!is.na(latitude_decdeg), !is.na(longitude_decdeg)) %>% 
-  tail(max_number_geocoded)
-
-
-message(paste0(Sys.time() , "created to_geocode"))
-
-
-# since the geocode python script reads data/wells.csv, we overwrite the 
-# wells.csv file with the data we want to geocode.
-write.csv(to_geocode %>%
-            rename(latitude_Decdeg = latitude_decdeg, 
-                   longitude_Decdeg = longitude_decdeg
-            )
-          , "data/wells.csv")
-
-if(nrow(to_geocode)>0){
-  message("Geocoding ", nrow(to_geocode)," rows. well_tag_number= ", paste(to_geocode$well_tag_number, collapse = " "))
-} else {message("no new wells to geocode")}
+if(update_csv){
+    max_number_geocoded <- 1000 # this is the maximum number of wells we are willing to geocode today
+    source("R/col_types_wells.R")
+    
+    
+    wells_csv <-
+      read_csv("https://raw.githubusercontent.com/bcgov/GWELLS-QAQC_Geocode_ArchiveData/main/data/wells.csv",
+               col_types = col_types_wells_janitor
+      )
+    
+    geocoded_csv <-       read_csv("https://raw.githubusercontent.com/bcgov/GWELLS-QAQC_Geocode_ArchiveData/main/data/wells_geocoded.csv",
+                                   col_types = col_types_geocoded_janitor
+    )
+    
+    
+    # read the historical caracteristics
+    #gwells_data_first_appearance <- read_csv("data/gwells_data_first_appearance.csv", col_types = col_types_wells)
+    wells_in_db <- wells_csv %>% select(well_tag_number, latitude_decdeg, longitude_decdeg)
+    
+    
+    # read the list of wells that have already been geocoded  
+    #geocoded <- read_csv("github_data/wells_geocoded.csv", col_types = col_types_geocoded)
+    wells_geocoded_in_db <- geocoded_csv %>% 
+      select(well_tag_number)
+    
+    
+    # this is the list of wells that need geocoding
+    to_geocode <- wells_in_db %>%
+      anti_join(wells_geocoded_in_db %>% head(-10)) %>%
+      filter(!is.na(latitude_decdeg), !is.na(longitude_decdeg)) %>% 
+      tail(max_number_geocoded)
+    
+    
+    message(paste0(Sys.time() , "created to_geocode"))
+    
+    
+    # since the geocode python script reads data/wells.csv, we overwrite the 
+    # wells.csv file with the data we want to geocode.
+    write.csv(to_geocode %>%
+                rename(latitude_Decdeg = latitude_decdeg, 
+                       longitude_Decdeg = longitude_decdeg
+                )
+              , "data/wells.csv")
+    
+    if(nrow(to_geocode)>0){
+      message("Geocoding ", nrow(to_geocode)," rows. well_tag_number= ", paste(to_geocode$well_tag_number, collapse = " "))
+    } else {message("no new wells to geocode")}
+    
+  
+  
+}
