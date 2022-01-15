30c2d97cf3a39c90958f1495de11700b8d336852
Simon Coulombe <simoncoulombe@gmail.com>
2022-01-14 19:20:08 -0500

    repopulate csv

--- R/repopulate_csv_from_sql.R
+++ R/repopulate_csv_from_sql.R
@@ -17,12 +17,17 @@ con1 <- DBI::dbConnect(
 wells <- dbGetQuery(con1, "select * from wells") 
 wells_geocoded <- dbGetQuery(con1, "select * from wells_geocoded") 
 wells_qa <- dbGetQuery(con1, "select * from wells_qa") 
-
 drilling_method <- dbGetQuery(con1, "select * from drilling_method") 
 
 DBI::dbDisconnect(con1)
 
-write_csv(wells %>% head(-10), "data/wells.csv")g
-write_csv(wells_geocoded %>% head(-10), "data/wells_geocoded.csv")
-write_csv(wells_qa %>% head(-10), "data/wells_qa.csv")
-write_csv(drilling_method %>% head(-10), "data/drilling_method.csv")
+
+wells2 <- wells %>% head(-10)
+wells_geocoded2 <- wells_geocoded %>% inner_join(wells %>% select(well_tag_number))
+wells_qa2 <- wells_qa %>% inner_join(wells %>% select(well_tag_number))
+drilling_method2 <- drilling_method  %>% inner_join(wells %>% select(well_tag_number))
+ 
+write_csv(wells2, "data/wells.csv")
+write_csv(wells_geocoded2, "data/wells_geocoded.csv")
+write_csv(wells_qa2, "data/wells_qa.csv")
+write_csv(drilling_method2, "data/drilling_method.csv")
